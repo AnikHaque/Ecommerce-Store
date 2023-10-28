d17c638b5029687c5f0f457dae1cc59c52dcdbbe
AnikHaque <ekramul15-811@diu.edu.bd>
2023-10-28 12:23:58 +0600

    updated in product details

--- src/components/product/ProductDetails.vue
+++ src/components/product/ProductDetails.vue
@@ -8,7 +8,6 @@ import useCartStore from '../../stores/cartStore';
 const cartStore = useCartStore();
 
 const router = useRouter()
-// const id = route.params.id
 const id = router.currentRoute.value.params.id;
 
 const loading = ref(true);
@@ -147,21 +146,12 @@ onMounted(async () => {
                 <div class="border-b border-gray-300">
                 <nav class="flex gap-4">
                     <a href="#" title="" class="border-b-2 border-gray-900 py-4 text-sm font-medium text-gray-900 hover:border-gray-400 hover:text-gray-800"> Description </a>
-
-                    <!-- <a href="#" title="" class="inline-flex items-center border-b-2 border-transparent py-4 text-sm font-medium text-gray-600">
-                    Reviews
-                    <span class="ml-2 block rounded-full bg-gray-500 px-2 py-px text-xs font-bold text-gray-100"> 1,209 </span>
-                    </a> -->
                 </nav>
                 </div>
 
                 <div class="mt-8 flow-root sm:mt-12">
                     <p>{{ productStore.product.description }}</p>
-                    <!-- <h1 class="text-3xl font-bold">Delivered To Your Door</h1>
-                    <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia accusantium nesciunt fuga.</p>
-                    <h1 class="mt-8 text-3xl font-bold">From the Fine Farms of Brazil</h1>
-                    <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio numquam enim facere.</p>
-                    <p class="mt-4">Amet consectetur adipisicing elit. Optio numquam enim facere. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore rerum nostrum eius facere, ad neque.</p> -->
+                    
                 </div>
             </div>
             </div>
